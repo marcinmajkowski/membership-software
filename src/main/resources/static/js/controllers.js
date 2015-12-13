03c61e45b1a985e00c83094ff24354f364b6db99
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-12-14 00:30:55 +0100

    Add very naive implementation for displaying table of last check-ins

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -10,11 +10,7 @@ angular.module('membershipManagementControllers', [])
     })
 
     .controller('CheckInCtrl', function ($scope, $http) {
-        $scope.code = null;
-        $scope.firstName = null;
-        $scope.lastName = null;
-        $scope.fullName = null;
-
+        //FIXME this is hell
         $scope.$on('scanEvent', function (event, message) {
             $scope.code = angular.fromJson(message.body);
 
@@ -31,7 +27,33 @@ angular.module('membershipManagementControllers', [])
                 $scope.lastName = null;
                 $scope.fullName = null;
             });
+
+            updateCheckIns();
         });
+
+        var updateCheckIns = function () {
+            $http.get('api/v1/checkIns?sort=timestamp,desc').then(function (response) {
+                $scope.checkIns = response.data._embedded.checkIns;
+
+                for (var i = 0; i < $scope.checkIns.length; i++) {
+                    (function (i) {
+                        $http.get($scope.checkIns[i]._links.card.href).then(function (response) {
+                            $scope.checkIns[i].code = response.data.code;
+                            $http.get(response.data._links.owner.href).then(function (response) {
+                                $scope.checkIns[i].firstName = response.data.firstName;
+                                $scope.checkIns[i].lastName = response.data.lastName;
+                            }, function (error) {
+                            })
+                        }, function (error) {
+                        });
+                    })(i);
+                }
+            }, function (error) {
+                $scope.checkIns = null;
+            });
+        };
+
+        updateCheckIns();
     })
 
     .controller('PaymentCtrl', function ($scope) {
