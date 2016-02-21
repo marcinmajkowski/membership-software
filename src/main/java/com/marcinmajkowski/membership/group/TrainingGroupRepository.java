ffe496411fda69c3320fa41453a0e2cfd73df956
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-02-21 23:25:33 +0100

    Add missing RepositoryRestResource annotation on TrainingGroupRepository class

--- src/main/java/com/marcinmajkowski/membership/group/TrainingGroupRepository.java
+++ src/main/java/com/marcinmajkowski/membership/group/TrainingGroupRepository.java
@@ -1,9 +1,11 @@
 package com.marcinmajkowski.membership.group;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by Marcin on 10/01/2016.
  */
+@RepositoryRestResource
 public interface TrainingGroupRepository extends CrudRepository<TrainingGroup, Long> {
 }
