d875c0d594c8fcf15d9e0cdd41f157454135fd1b
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-08-07 15:20:01 +0200

    Remove Bootstrap front end and start working with Angular Material (material-starter)

---
+++ src/main/resources/webapp/app/src/home/HomeController.js
@@ -0,0 +1,21 @@
+(function(){
+
+  angular
+       .module('home')
+       .controller('HomeController', [
+          HomeController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function HomeController() {
+    var self = this;
+
+  }
+
+})();
---
+++ src/main/resources/webapp/app/src/payments/PaymentController.js
@@ -0,0 +1,21 @@
+(function(){
+
+  angular
+       .module('payments')
+       .controller('PaymentController', [
+          PaymentController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function PaymentController() {
+    var self = this;
+
+  }
+
+})();
