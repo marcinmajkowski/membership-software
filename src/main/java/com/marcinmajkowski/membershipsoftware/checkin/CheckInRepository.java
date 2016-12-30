ab857b30c2089460c21b4dd1ca48fcfde6b49d85
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-12-30 02:36:35 +0100

    Add pageable findByCustomer to checkIn and payment repositories

--- src/main/java/com/marcinmajkowski/membershipsoftware/checkin/CheckInRepository.java
+++ src/main/java/com/marcinmajkowski/membershipsoftware/checkin/CheckInRepository.java
@@ -1,6 +1,8 @@
 package com.marcinmajkowski.membershipsoftware.checkin;
 
 import com.marcinmajkowski.membershipsoftware.customer.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,6 +11,8 @@ import java.util.List;
 
 @RepositoryRestResource
 public interface CheckInRepository extends PagingAndSortingRepository<CheckIn, Long> {
+
+    Page<CheckIn> findByCustomer(@Param("customer") Customer customer, Pageable pageable);
 
     // finding last visit for a card
     CheckIn findFirstByCardCodeOrderByTimestampDesc(String code);
